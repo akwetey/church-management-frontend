2d80578b7a0358f1ac975a8cef9dabcee2a16032
Nana Kwesi Ofosu-Aikins <61139144+nanaaikinson@users.noreply.github.com>
2020-08-08 09:38:18 +0000

    Update: Dashboard

--- src/router/views/dashboard/Index.vue
+++ src/router/views/dashboard/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- Statts -->
     <div class="row mb-4">
       <div class="col-md-3">
         <div class="card py-2">
@@ -17,7 +18,7 @@
           <div class="card-body">
             <div class="text-center">
               <h3 class="display-3">{{total.families}}</h3>
-              <h5>Family</h5>
+              <h5>Families</h5>
             </div>
           </div>
         </div>
@@ -46,18 +47,60 @@
       </div>
     </div>
 
+    <!--  -->
     <div class="row">
       <div class="col-md-9">
-        <div class="card">
+        <div class="card" style="min-height: 400px;">
           <div class="card-body">
-            <h5>Recently added people</h5>
+            <h5 class="text-center">Recently added people</h5>
+
+            <div class="table-responsive mt-5">
+              <table class="table table-hover mb-0">
+                <thead>
+                  <tr>
+                    <th>Person</th>
+                    <th>Telephone</th>
+                    <th>Family Relation</th>
+                    <th>Status</th>
+                    <th>Date Creatd</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr v-for="person in people" :key="person.mask">
+                    <td>
+                      <div class="d-flex align-items-center">
+                        <div class="mr-3">
+                          <img
+                            :src="person.avatar ? person.avatar : defaultAvatar"
+                            class="rounded-circle wd-35"
+                            alt="user"
+                          />
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                          <p class="text-muted">{{ person.name }}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>{{ person.primary_telephone }}</td>
+                    <td>{{ person.family_relation }}</td>
+                    <td>{{ person.member_status }}</td>
+                    <td>{{ person.created_at }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
 
       <div class="col-md-3">
-        <div class="card">
-          <div class="card-body"></div>
+        <div class="card" style="height: 400px;">
+          <div class="card-body">
+            <h5 class="text-center mb-4">Gender Ratio</h5>
+            <ApexChart width="100%" type="donut" :options="chart.options" :series="chart.series"></ApexChart>
+          </div>
         </div>
       </div>
     </div>
@@ -66,21 +109,65 @@
 
 <script>
 import Dashboard from "@services/api/dashboard";
+import VueApexCharts from "vue-apexcharts";
 
 export default {
   name: "Dashboard",
+  components: {
+    ApexChart: VueApexCharts,
+  },
   data() {
     return {
       total: {},
       people: [],
-      gender: {},
+      chart: {
+        options: {
+          legend: {
+            position: "bottom",
+          },
+          plotOptions: {
+            pie: {
+              donut: {
+                size: "65%",
+                labels: {
+                  show: true,
+                  name: {
+                    color: "#333",
+                  },
+                  value: {
+                    formatter: function (val) {
+                      return val;
+                    },
+                  },
+                  total: {
+                    show: true,
+                    formatter: function (w) {
+                      return w.globals.seriesTotals.reduce((a, b) => {
+                        return a + b;
+                      }, 0);
+                    },
+                  },
+                },
+              },
+            },
+          },
+          labels: ["Male", "Female"],
+        },
+        series: [],
+      },
     };
+  },
+  computed: {
+    defaultAvatar() {
+      return this.$store.getters.defaultAvatar;
+    },
   },
   methods: {
     setData(payload) {
       this.total = payload.total;
       this.people = payload.people;
-      this.gender = payload.gender;
+      this.chart.series.push(payload.gender.male);
+      this.chart.series.push(payload.gender.female);
     },
   },
   async beforeRouteEnter(to, from, next) {
